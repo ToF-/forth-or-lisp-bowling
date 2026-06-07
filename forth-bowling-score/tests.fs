4850287980f5f7b4694d748af7192eaf34b8a3d6
ToF- <cthibauttof@gmail.com>
2026-06-07 22:10:29 +0200

    forth added tests

--- forth-bowling-score/tests.fs
+++ forth-bowling-score/tests.fs
@@ -1,18 +1,23 @@
 require bowling.fs
 require ffl/tst.fs
 page
+    : +r +roll ;
     ." no rolls then score is zero" cr
     start score @ 0 ?S
 
     ." adding rolls increases score" cr
-    start 3 +roll 2 +roll 4 +roll score @ 9 ?S
+    start 3 +r 2 +r 4 +r score @ 9 ?S
 
     ." closing a frame with a spare generates bonus" cr
-    start 4 +roll 6 +roll 3 +roll score @ 16 ?s
+    start 4 +r 6 +r 3 +r score @ 16 ?s
 
     ." closing a frame with a strike generates 2 boni" cr
-    start 10 +roll 5 +roll 1 +roll score @ 22 ?s
+    start 10 +r 5 +r 1 +r score @ 22 ?s
 
     ." after ten frames, no rolls are added except bonus" cr
-    start 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll 10 +roll score @ 300 ?s
+    start 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r score @ 300 ?s
+
+    ." some games" cr
+    start 4 +r 5 +r 4 +r 6 +r 3 +r 4 +r 10 +r 8 +r 1 +r 10 +r 6 +r 3 +r 7 +r 2 +r 10 +r 10 +r 1 +r 2 +r score @ 128 ?s
+    start 5 +r 5 +r 2 +r 8 +r 3 +r 7 +r 4 +r 6 +r 5 +r 5 +r 4 +r 6 +r 3 +r 7 +r 2 +r 8 +r 1 +r 9 +r 0 +r 10 +r 6 +r score @ 130 ?s
 bye
