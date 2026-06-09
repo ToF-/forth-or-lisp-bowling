d54263fad8b2d20d650d9e2a168ac7db0df1ce1f
ToF- <cthibauttof@gmail.com>
2026-06-09 18:21:52 +0200

    haskell added tests

--- haskell-bowling-score/test/Spec.hs
+++ haskell-bowling-score/test/Spec.hs
@@ -10,3 +10,11 @@ main = hspec $ do
             score [4,5,3,2] `shouldBe` 14
         it "adds a bonus when given a spare" $ do
             score [6,4,2] `shouldBe` 14
+        it "adds two boni when given a strike" $ do
+            score [10,4,4] `shouldBe` 26
+        it "counts points and boni until frame ten" $ do
+            score [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10] `shouldBe` 300
+            score [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5] `shouldBe` 150
+        it "should pass on some games" $ do
+            score [5, 5, 2, 8, 3, 7, 4, 6, 5, 5, 4, 6, 3, 7, 2, 8, 1, 9, 0, 10, 6] `shouldBe` 130
+            score [4, 5, 4, 6, 3, 4, 10, 8, 1, 10, 6, 3, 7, 2, 10, 10, 1, 2] `shouldBe` 128
