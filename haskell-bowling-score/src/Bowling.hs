ed4f4bac13583430cc7ffd0b63cb904015922acf
Christophe Thibaut <cthibauttof@gmail.com>
2026-06-09 17:29:57 +0200

    haskell correct

--- haskell-bowling-score/src/Bowling.hs
+++ haskell-bowling-score/src/Bowling.hs
@@ -1,30 +1,38 @@
 module Bowling
     where
 
+type Bonus = Int
 type Roll = Int
 data Frame = NoFrame
            | Open Roll
            | Frame Roll Roll
-           | Spare Roll
-           | Strike
-
+           | Spare Roll Bonus
+           | Strike Bonus Bonus
     deriving (Eq, Show)
+
+points :: Frame -> Int
+points NoFrame = 0
+points (Open r) = r
+points (Frame x y) = x + y
+points (Spare r b) = 10 + b
+points (Strike a b) = 10 + a + b
 
 qualify :: [Int] -> (Frame, [Int])
 qualify [] = (NoFrame, [])
-qualify (x:rest) | x == 10 = (Strike, rest)
-qualify (x:y:rest) | x + y == 10 = (Spare x, rest)
+qualify [x,y] | x == 10 = (Strike y 0, [])
+qualify (x:y:z:rest) | x == 10 = (Strike y z, y:z:rest)
+qualify [x,y] | x + y == 10 = (Spare y 0, [])
+qualify (x:y:z:rest) | x + y == 10 = (Spare y z, z:rest)
 qualify (x:y:rest) = (Frame x y, rest)
 qualify (x:rest) = (Open x, rest)
 
+qualify_at :: [Int] -> Int -> [Frame]
+qualify_at _ 10 = []
+qualify_at [] _ = []
+qualify_at rolls f = let (frame, rest) = qualify rolls in
+                         frame : (qualify_at rest (succ f))
 score :: [Int] -> Int
-score rolls = let (q,rest) = qualify rolls in
-                case q of
-                  Strike -> 0
-                  Spare x -> 10 + (head rest) + score rest
-                  Frame x y -> x + y + score rest
-                  Open x -> x + score rest
-                  NoFrame -> 0
+score rolls = foldl (\acc q -> acc + points q) 0 (qualify_at rolls 0)
 
 
 
