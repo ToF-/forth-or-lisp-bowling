2540048a447332edd8456e759d913659acdb852a
Christophe Thibaut <cthibauttof@gmail.com>
2026-06-09 20:12:19 +0200

    haskell refactoring

--- haskell-bowling-score/src/Bowling.hs
+++ haskell-bowling-score/src/Bowling.hs
@@ -1,4 +1,4 @@
-module Bowling
+module Bowling (score)
     where
 
 type Bonus = Int
@@ -14,25 +14,26 @@ points :: Frame -> Int
 points NoFrame = 0
 points (Open r) = r
 points (Frame x y) = x + y
-points (Spare r b) = 10 + b
+points (Spare _ b) = 10 + b
 points (Strike a b) = 10 + a + b
 
 qualify :: [Int] -> (Frame, [Int])
 qualify [] = (NoFrame, [])
 qualify [x,y] | x == 10 = (Strike y 0, [])
-qualify (x:y:z:rest) | x == 10 = (Strike y z, y:z:rest)
 qualify [x,y] | x + y == 10 = (Spare y 0, [])
+qualify (x:y:z:rest) | x == 10 = (Strike y z, y:z:rest)
 qualify (x:y:z:rest) | x + y == 10 = (Spare y z, z:rest)
 qualify (x:y:rest) = (Frame x y, rest)
 qualify (x:rest) = (Open x, rest)
 
-qualify_at :: [Int] -> Int -> [Frame]
-qualify_at _ 10 = []
-qualify_at [] _ = []
-qualify_at rolls f = let (frame, rest) = qualify rolls in
-                         frame : (qualify_at rest (succ f))
+frames :: [Int] -> Int -> [Frame]
+frames _ 10 = []
+frames [] _ = []
+frames rolls f = frame : frames rest (succ f)
+    where (frame, rest) = qualify rolls
+
 score :: [Int] -> Int
-score rolls = foldl (\acc q -> acc + points q) 0 (qualify_at rolls 0)
+score rolls = foldl (\acc q -> acc + points q) 0 (frames rolls 0)
 
 
 
