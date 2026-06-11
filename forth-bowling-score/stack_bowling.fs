6cb8647c5f3a53d4737e2b78281fdf9b1d130a53
Christophe Thibaut <cthibauttof@gmail.com>
2026-06-11 16:12:48 +0200

    stacked forth bowling wip

--- forth-bowling-score/stack_bowling.fs
+++ forth-bowling-score/stack_bowling.fs
@@ -2,12 +2,8 @@
     dup 3 and
     swap 2/ 2/ swap ;
 
-: collect-bonus  ( frame#,frame,bonus,score,roll,roll -- frame#,frame,bonus,score,roll )
-    >r rot bonus>> >r -rot
-    r> r> * + ;
-
-: update-score ( score,roll,roll -- score',roll )
-    rot + swap ;
+: update-score ( score,roll -- score',roll )
+    + ;
 
 : new-frame? ( frame -- f )
     0= ;
@@ -22,41 +18,60 @@
     2swap drop 1+ 0 2swap ;
 
 : open-frame ( frame#,frame,bonus,score,roll -- frame#,frame',bonus,score )
-    -rot 2swap nip 1+ 2swap ;
+    >r 2swap drop r> 1+    \ bonus,score,frame#,frame'
+    2swap ; 
 
 : check-strike ( frame#,frame,bonus,score,roll -- frame#,frame,bonus,score,roll )
     dup 10 = if
-        drop
-        strike!
+        drop strike!
         close-frame
     else
         open-frame
     then ;
 
+: frame ( frame#,frame,bonus,score,roll -- …,frame )
+    2swap over >r 2swap r> ;
+
 : last-roll ( frame -- roll )
     1- ;
 
 : check-spare ( frame#,frame,bonus,score,roll -- frame#,frame,bonus,score )
-    >r rot >r -rot r> last-roll r> + 10 = if spare! then
+    frame last-roll 
+    + 10 = if spare! then
     close-frame ;
+
 : check-bonus ( frame#,frame,bonus,score,roll -- frame#,frame,bonus,score )
-    2swap over >r 2swap r> new-frame? if
+    frame new-frame? if
         check-strike
     else
         check-spare
     then ;
 
+: in-game? ( frame# -- f )
+    0 10 within ;
+
+: .state
+    2over swap
+    ." frame#:" . ." frame:" .
+    2dup swap
+    ." bonus:" . ." score:" . 
+    cr ;
+
+: collect-bonus  ( frame#,frame,bonus,score,roll -- frame#,frame,bonus,score,roll )
+    >r swap bonus>>        \ frame#,frame,score,bonus',factor
+    r@ * rot + r> ;        \ frame#,frame,bonus',score',roll ;
+
 : +roll ( frame#,frame,bonus,score,roll - frame#',frame',bonus',score' )
-   dup collect-bonus 
-   2swap over >r 2swap
-   r> 0 10 within if
-      dup update-score
-      check-bonus
+   collect-bonus
+   2swap over >r 2swap r>
+   in-game? if
+      dup check-bonus
+      update-score
    else
        drop
-   then
+   then .state ;
 
-: start ( frame#',frame',bonus',score' )
+: start ( -- frame#,frame,bonus,score )
     0 0 0 0 ;
 
 : score ( frame#',frame',bonus',score' )
--- forth-bowling-score/tests.fs
+++ forth-bowling-score/tests.fs
@@ -1,23 +1,24 @@
-require bowling.fs
+require stack_bowling.fs
 require ffl/tst.fs
 page
     : +r +roll ;
     ." no rolls then score is zero" cr
-    start score @ 0 ?S
+    start score 0 ?S
 
     ." adding rolls increases score" cr
-    start 3 +r 2 +r 4 +r score @ 9 ?S
+    start 3 +r 2 +r 4 +r score 9 ?S
 
     ." closing a frame with a spare generates bonus" cr
-    start 4 +r 6 +r 3 +r score @ 16 ?s
+    start 4 +r 6 +r 3 +r score 16 ?s
+    bye
 
     ." closing a frame with a strike generates 2 boni" cr
-    start 10 +r 5 +r 1 +r score @ 22 ?s
+    start 10 +r 5 +r 1 +r score 22 ?s
 
     ." after ten frames, no rolls are added except bonus" cr
-    start 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r score @ 300 ?s
+    start 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r 10 +r score 300 ?s
 
     ." some games" cr
-    start 4 +r 5 +r 4 +r 6 +r 3 +r 4 +r 10 +r 8 +r 1 +r 10 +r 6 +r 3 +r 7 +r 2 +r 10 +r 10 +r 1 +r 2 +r score @ 128 ?s
-    start 5 +r 5 +r 2 +r 8 +r 3 +r 7 +r 4 +r 6 +r 5 +r 5 +r 4 +r 6 +r 3 +r 7 +r 2 +r 8 +r 1 +r 9 +r 0 +r 10 +r 6 +r score @ 130 ?s
+    start 4 +r 5 +r 4 +r 6 +r 3 +r 4 +r 10 +r 8 +r 1 +r 10 +r 6 +r 3 +r 7 +r 2 +r 10 +r 10 +r 1 +r 2 +r score 128 ?s
+    start 5 +r 5 +r 2 +r 8 +r 3 +r 7 +r 4 +r 6 +r 5 +r 5 +r 4 +r 6 +r 3 +r 7 +r 2 +r 8 +r 1 +r 9 +r 0 +r 10 +r 6 +r score 130 ?s
 bye
